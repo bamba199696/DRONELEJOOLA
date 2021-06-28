4aec3fbbab686a538ebaa76aabd624cf7aa8bec9
khadim mbacke ndiaye <83848150+bamba199696@users.noreply.github.com>
2021-06-28 18:02:09 +0000

    Add files via upload

---
+++ mors.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+from code import *
+pinNum = 26
+GPIO.setmode(GPIO.BCM) #numbering scheme that corresponds to breakout board and pin layout
+GPIO.setup(pinNum,GPIO.OUT) #replace pinNum with whatever pin you used, this sets up that pin as an output
+#set LED to flash forever
+#while True:
+#  GPIO.output(pinNum,GPIO.HIGH)
+#  time.sleep(0.5)
+#  GPIO.output(pinNum,GPIO.LOW)
+#  time.sleep(0.5)
+
+
+antwoord=raw_input('Wat wil je in morse seinen?')
+for letter in antwoord:
+	morse=(CODE[letter.upper()])
+	
+
+
+GPIO.cleanup()
+
